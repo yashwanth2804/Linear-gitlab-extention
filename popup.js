5b3c07a568b189ebdacac4266b5ef78827686e5a
yashwanth2804 <kambaalayashwanth@outlook.com>
2024-08-05 21:33:58 +0530

    Added Identifier branch

--- popup.js
+++ popup.js
@@ -3,18 +3,25 @@ document.getElementById('urlForm').addEventListener('submit', function (e) {
     e.preventDefault();
 
     var gitlabUrl = document.getElementById('gitlabUrl').value;
+    var gitlabUrlSelect = document.getElementById('gitlabUrlSelect').value;
+
 
     // Save the URL to Chrome storage
-    chrome.storage.sync.set({ gitlabUrl: gitlabUrl }, function () {
-        document.getElementById('status').textContent = 'URL saved!';
+    chrome.storage.sync.set({ gitlabUrl: gitlabUrl, gitlabUrlSelect: gitlabUrlSelect }, function () {
+        document.getElementById('status').textContent = 'Data saved!';
+
     });
 });
 
 // Load the saved URL when the popup is opened
 document.addEventListener('DOMContentLoaded', function () {
-    chrome.storage.sync.get(['gitlabUrl'], function (result) {
+    chrome.storage.sync.get(['gitlabUrl', 'gitlabUrlSelect'], function (result) {
         if (result.gitlabUrl) {
             document.getElementById('gitlabUrl').value = result.gitlabUrl;
+        }
+
+        if (result.gitlabUrlSelect) {
+            document.getElementById('gitlabUrlSelect').value = result.gitlabUrlSelect;
         }
     });
 });
