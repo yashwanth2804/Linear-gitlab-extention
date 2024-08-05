75b57dc6973f2d5397b2a790046eff834f234edf
yashwanth2804 <kambaalayashwanth@outlook.com>
2024-08-05 22:28:15 +0530

    Remove un logs

--- background.js
+++ background.js
@@ -2,10 +2,8 @@
 
 // Listen for URL changes in the tabs
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-    console.log("@@ changeInfo URL updates", changeInfo)
     // Check if the status is 'complete' to ensure the page has fully loaded
     if (changeInfo.status === 'complete') {
-
         // Send a message to the content script to update the page
         chrome.tabs.sendMessage(tabId, { action: 'updateContent', url: tab.url });
     }
