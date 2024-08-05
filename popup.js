9f5ef4c2ecb0049bdb4617f24a7b8a3b69b439c1
yashwanth2804 <kambaalayashwanth@outlook.com>
2024-08-05 21:58:35 +0530

    added username logic

--- popup.js
+++ popup.js
@@ -3,25 +3,28 @@ document.getElementById('urlForm').addEventListener('submit', function (e) {
     e.preventDefault();
 
     var gitlabUrl = document.getElementById('gitlabUrl').value;
-    var gitlabUrlSelect = document.getElementById('gitlabUrlSelect').value;
-
+    var branchFormat = document.getElementById('branchFormat').value;
+    var username = document.getElementById('username').value;
 
     // Save the URL to Chrome storage
-    chrome.storage.sync.set({ gitlabUrl: gitlabUrl, gitlabUrlSelect: gitlabUrlSelect }, function () {
+    chrome.storage.sync.set({ gitlabUrl: gitlabUrl, branchFormat: branchFormat, username: username }, function () {
         document.getElementById('status').textContent = 'Data saved!';
-
     });
 });
 
 // Load the saved URL when the popup is opened
 document.addEventListener('DOMContentLoaded', function () {
-    chrome.storage.sync.get(['gitlabUrl', 'gitlabUrlSelect'], function (result) {
+    chrome.storage.sync.get(['gitlabUrl', 'branchFormat'], function (result) {
         if (result.gitlabUrl) {
             document.getElementById('gitlabUrl').value = result.gitlabUrl;
         }
 
-        if (result.gitlabUrlSelect) {
-            document.getElementById('gitlabUrlSelect').value = result.gitlabUrlSelect;
+        if (result.branchFormat) {
+            document.getElementById('branchFormat').value = result.branchFormat;
+        }
+
+        if (result.username) {
+            document.getElementById('username').value = result.username;
         }
     });
 });
