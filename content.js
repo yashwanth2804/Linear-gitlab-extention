824a3a185a0e83523145ffc084291ed05406230d
yashwanth2804 <kambaalayashwanth@outlook.com>
2024-08-11 16:22:04 +0530

    Added logic to work for github

--- content.js
+++ content.js
@@ -2,7 +2,8 @@ const DEFAULT_MAX_LENGTH = 60;
 
 // Configuration
 const config = {
-    gitlabUrl: '',
+    selectedService: '',
+    gitUrl: '',
     branchFormat: '',
     username: ''
 };
@@ -10,16 +11,27 @@ const config = {
 // DOM Selectors
 const selectors = {
     copyIssueButton: 'button[aria-label="Issue options"]',
-    existingLink: 'a.gitlab-link'
+    existingLink: 'a.git-link'
 };
 
 // Main function
 function addGitLabLink(pageUrl) {
-    chrome.storage.sync.get(['gitlabUrl', 'branchFormat', 'username'], (result) => {
+    chrome.storage.sync.get(['selectedService', 'gitUrl', 'branchFormat', 'username'], (result) => {
         Object.assign(config, result);
         const branchIdentifier = createBranchIdentifier(pageUrl);
-        const gitlabUrl = createGitlabUrl(branchIdentifier);
-        appendOrUpdateGitLabLink(gitlabUrl);
+        let gitUrl = "";
+        // check git service if github or gitlab
+        if (config.selectedService === 'github') {
+            gitUrl = createGitHubUrl(branchIdentifier);
+            appendOrUpdateGitLabLink(gitUrl, 'github');
+
+        }
+
+        if (config.selectedService === 'gitlab') {
+            gitUrl = createGitlabUrl(branchIdentifier);
+            appendOrUpdateGitLabLink(gitUrl, 'gitlab');
+
+        }
     });
 }
 
@@ -50,24 +62,29 @@ function getBranchFormat(identifier, title) {
 
 // GitLab URL creation
 function createGitlabUrl(branchIdentifier) {
-    const baseUrl = trimTrailingSlash(config.gitlabUrl);
+    const baseUrl = trimTrailingSlash(config.gitUrl);
     return `${baseUrl}/-/tree/${branchIdentifier}`;
 }
 
+function createGitHubUrl(branchIdentifier) {
+    const baseUrl = trimTrailingSlash(config.gitUrl);
+    return `${baseUrl}/tree/${branchIdentifier}`;
+}
+
 // DOM manipulation
-function appendOrUpdateGitLabLink(gitlabUrl) {
+function appendOrUpdateGitLabLink(_gitUrl, _service) {
     const appendGitLabLink = () => {
         const copyIssueButton = document.querySelector(selectors.copyIssueButton);
         if (!copyIssueButton) return;
 
         const parentNode = copyIssueButton.parentNode.parentNode;
-        let gitLabLink = parentNode.querySelector(selectors.existingLink);
+        let gitLinkNode = parentNode.querySelector(selectors.existingLink);
 
-        if (!gitLabLink) {
-            gitLabLink = createGitLabLinkElement(gitlabUrl);
-            parentNode.insertBefore(gitLabLink, copyIssueButton.parentNode.nextSibling);
+        if (!gitLinkNode) {
+            gitLinkNode = _service === 'github' ? createGitHubLinkElement(_gitUrl) : createGitLabLinkElement(_gitUrl);
+            parentNode.insertBefore(gitLinkNode, copyIssueButton.parentNode.nextSibling);
         } else {
-            gitLabLink.href = gitlabUrl;
+            gitLinkNode.href = _gitUrl;
         }
 
         observer.disconnect();
@@ -81,7 +98,7 @@ function appendOrUpdateGitLabLink(gitlabUrl) {
 // GitLab link element creation
 function createGitLabLinkElement(url) {
     const link = document.createElement('a');
-    link.className = 'gitlab-link';
+    link.className = 'git-link';
     link.href = url;
     link.target = '_blank';
     link.style.marginLeft = '10px';
@@ -94,6 +111,26 @@ function createGitLabLinkElement(url) {
 
     container.appendChild(svgContainer);
     container.appendChild(document.createTextNode(' GitLab'));
+    link.appendChild(container);
+
+    return link;
+}
+
+function createGitHubLinkElement(url) {
+    const link = document.createElement('a');
+    link.className = 'git-link';
+    link.href = url;
+    link.target = '_blank';
+    link.style.marginLeft = '10px';
+
+    const container = document.createElement('span');
+    const svgContainer = document.createElement('div');
+    svgContainer.innerHTML = getGitHubSvg();
+    svgContainer.style.display = 'inline-block';
+    svgContainer.style.verticalAlign = 'middle';
+
+    container.appendChild(svgContainer);
+    container.appendChild(document.createTextNode(' GitHub'));
     link.appendChild(container);
 
     return link;
@@ -120,6 +157,14 @@ function getGitLabSvg() {
         <path d="M238.28027,152.50246,210.95068,50.01662a12.00021,12.00021,0,0,0-22.83056-1.12109L167.45605,104H88.544L67.87988,48.89651a12.00012,12.00012,0,0,0-22.831,1.12109L17.71973,152.50246a16.03981,16.03981,0,0,0,6.35254,17.27783l94.82031,65.64454a16.07612,16.07612,0,0,0,18.21484,0l94.82031-65.64454A16.03981,16.03981,0,0,0,238.28027,152.50246ZM128,222.26955,33.17969,156.625,57.2876,66.21877,75.50928,114.8091A7.99963,7.99963,0,0,0,83,120h90a7.99963,7.99963,0,0,0,7.49072-5.19092L198.7124,66.21877,222.82031,156.625Z"></path>
     </svg>`;
 }
+
+function getGitHubSvg() {
+    return `<svg fill="#000000" width="24px" height="24px" viewBox="0 0 256 256" id="Flat" xmlns="http://www.w3.org/2000/svg">
+  <path d="M200,112v8a47.99987,47.99987,0,0,1-48,48H104a47.99987,47.99987,0,0,1-48-48v-8a49.25725,49.25725,0,0,1,8.51172-27.29639A51.90015,51.90015,0,0,1,68,40a51.96059,51.96059,0,0,1,43.8252,23.99927l32.3496.00049A51.95917,51.95917,0,0,1,188,40a51.90015,51.90015,0,0,1,3.48828,44.70361A49.25725,49.25725,0,0,1,200,112Z" opacity="0.2"/>
+  <path d="M216,216a16.01833,16.01833,0,0,1-16-16v-8a31.9949,31.9949,0,0,0-14.78076-26.95068A55.951,55.951,0,0,0,208,120v-8a58.04124,58.04124,0,0,0-7.69531-28.32031A59.73339,59.73339,0,0,0,194.92822,36,7.99908,7.99908,0,0,0,188,32a59.74792,59.74792,0,0,0-48.00781,24l-23.98487-.001A59.74952,59.74952,0,0,0,68,32a7.99908,7.99908,0,0,0-6.92822,4,59.73551,59.73551,0,0,0-5.377,47.67969A58.0419,58.0419,0,0,0,48,112v8a55.951,55.951,0,0,0,22.78076,45.04932A31.9949,31.9949,0,0,0,56,192v8a16.01833,16.01833,0,0,1-16,16,8,8,0,0,0,0,16,32.03667,32.03667,0,0,0,32-32v-8a16.01833,16.01833,0,0,1,16-16h12v40a16.01833,16.01833,0,0,1-16,16,8,8,0,0,0,0,16,32.03667,32.03667,0,0,0,32-32V176h24v40a32.03667,32.03667,0,0,0,32,32,8,8,0,0,0,0-16,16.01833,16.01833,0,0,1-16-16V176h12a16.01833,16.01833,0,0,1,16,16v8a32.03667,32.03667,0,0,0,32,32,8,8,0,0,0,0-16ZM64,120v-8A41.77977,41.77977,0,0,1,70.90088,89.5176a7.99937,7.99937,0,0,0,1.07666-7.6875,43.82564,43.82564,0,0,1,.78906-33.5752,43.84232,43.84232,0,0,1,32.32129,20.05762,7.99912,7.99912,0,0,0,6.73682,3.68652L144.17432,72h.00048a8.00058,8.00058,0,0,0,6.73731-3.68652A43.84259,43.84259,0,0,1,183.2334,48.2549a43.8234,43.8234,0,0,1,.78857,33.5752,8.08338,8.08338,0,0,0,1.04932,7.65039A41.7664,41.7664,0,0,1,192,112v8a40.04551,40.04551,0,0,1-40,40H104A40.04551,40.04551,0,0,1,64,120Z"/>
+</svg>`;
+}
+
 
 // Message listener
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
--- popup.js
+++ popup.js
@@ -1,13 +1,14 @@
 // popup.js
 document.getElementById('urlForm').addEventListener('submit', function (e) {
     e.preventDefault();
+    const selectedService = document.querySelector('input[name="gitService"]:checked').value;
+    const gitUrl = document.getElementById('gitUrl').value;
 
-    var gitlabUrl = document.getElementById('gitlabUrl').value;
     var branchFormat = document.getElementById('branchFormat').value;
     var username = document.getElementById('username').value;
 
     // Save the URL to Chrome storage
-    chrome.storage.sync.set({ gitlabUrl: gitlabUrl, branchFormat: branchFormat, username: username }, function () {
+    chrome.storage.sync.set({ selectedService: selectedService, gitUrl: gitUrl, branchFormat: branchFormat, username: username }, function () {
         document.getElementById('status').textContent = 'Data saved!';
         // Show the hint to reload
         document.getElementById('hint').style.display = 'block';
@@ -17,9 +18,13 @@ document.getElementById('urlForm').addEventListener('submit', function (e) {
 
 // Load the saved URL when the popup is opened
 document.addEventListener('DOMContentLoaded', function () {
-    chrome.storage.sync.get(['gitlabUrl', 'branchFormat', 'username'], function (result) {
-        if (result.gitlabUrl) {
-            document.getElementById('gitlabUrl').value = result.gitlabUrl;
+    chrome.storage.sync.get(['selectedService', 'gitUrl', 'branchFormat', 'username'], function (result) {
+        if (result.selectedService) {
+            document.getElementById(result.selectedService).checked = true;
+        }
+
+        if (result.gitUrl) {
+            document.getElementById('gitUrl').value = result.gitUrl;
         }
 
         if (result.branchFormat) {
