78e9f43b5a2f2919d022d5de633769a13fbf570b
yashwanth2804 <kambaalayashwanth@outlook.com>
2024-08-26 13:06:49 +0530

    fixed unknown listner bug

--- background.js
+++ background.js
@@ -4,7 +4,18 @@
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
     // Check if the status is 'complete' to ensure the page has fully loaded
     if (changeInfo.status === 'complete') {
-        // Send a message to the content script to update the page
-        chrome.tabs.sendMessage(tabId, { action: 'updateContent', url: tab.url });
+        chrome.tabs.get(tabId, function (tab) {
+            if (chrome.runtime.lastError) {
+                console.log(chrome.runtime.lastError.message);
+            } else {
+                chrome.tabs.sendMessage(tabId, { action: 'updateContent', url: tab.url }, function (response) {
+                    if (chrome.runtime.lastError) {
+                        console.log('Error sending message:', chrome.runtime.lastError.message);
+                    } else if (response) {
+                        console.log('Message sent successfully');
+                    }
+                });
+            }
+        });
     }
 });
